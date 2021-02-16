9c847b50530e098c03dbe323f8d16e70fd0156b1
Wardayaz <wayaz107@gmail.com>
2021-02-16 11:41:00 -0600

    added destroy and strong params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,16 +2,28 @@ class BooksController < ApplicationController
 
     def index
         @books = Book.all
-        render json: @books
+        render json: @books, only: [:id, :title, :author, :image]
     end 
 
     def create
+        @book = Book.create(book_params)
+        render json: @book
     end 
 
     def show
+        @book = Book.find(params[:id])
+        render json: @book
     end
 
     def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+    end 
+
+    private
+
+    def book_params
+        params.require(:book).permit(:title,:author,:image)
     end 
 
 end
