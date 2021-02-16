8cf86756a9ce2607af2cc5ae1ec448aa1b7e2617
Wardayaz <wayaz107@gmail.com>
2021-02-15 21:47:05 -0600

    migrated files

--- db/migrate/20210216025043_create_books.rb
+++ db/migrate/20210216025043_create_books.rb
@@ -1,6 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.string :image
 
       t.timestamps
     end
--- db/migrate/20210216025048_create_reviews.rb
+++ db/migrate/20210216025048_create_reviews.rb
@@ -1,7 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
-
+      t.text :content
+      t.string :reviewer
+      t.references :book, foreign_key: true
       t.timestamps
     end
   end
