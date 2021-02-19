4dbda8adb294ab2cea0d5b0ef06cf6d80cdd4349
Wardayaz <wayaz107@gmail.com>
2021-02-19 00:20:57 -0600

    added perfumes controller methods outline

--- app/controllers/api/v1/perfumes_controller.rb
+++ app/controllers/api/v1/perfumes_controller.rb
@@ -1,29 +1,17 @@
 class Api::V1::PerfumesController < ApplicationController
 
-    def index
-        @books = Book.all
-        render json: @books, only: [:id, :title, :author, :image]
-    end 
-
-    def create
-        @book = Book.create(book_params)
-        render json: @book
+    def index 
     end 
 
     def show
-        @book = Book.find(params[:id])
-        render json: @book
     end
 
-    def destroy
-        @book = Book.find(params[:id])
-        @book.destroy
+    def create 
     end 
 
     private
 
-    def book_params
-        params.require(:book).permit(:title,:author,:image)
+    def perfume_params
     end 
 
 end
