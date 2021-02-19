e7af7d2f3f91afba803b76ec577c01ef6076b8c9
Wardayaz <wayaz107@gmail.com>
2021-02-19 00:39:44 -0600

    updated create method

--- app/controllers/api/v1/perfumes_controller.rb
+++ app/controllers/api/v1/perfumes_controller.rb
@@ -12,6 +12,12 @@ class Api::V1::PerfumesController < ApplicationController
 
     def create 
         @perfume = Perfume.new(perfume_params)
+
+        if @perfume.save
+            render json: @perfume, status: :created
+          else
+            render json: @perfume.errors, status: :unprocessable_entity
+          end
     end 
 
     private
