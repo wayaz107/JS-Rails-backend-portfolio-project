42d88331fe49f75ed3943336986997bf4371b1eb
Wardayaz <wayaz107@gmail.com>
2021-02-19 01:36:29 -0600

    added seed data and migrated files

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,15 @@
 
 # Book.destroy_all
 # Review.destroy_all
+
+perfume1 = Perfume.create(name: 'COCO MADEMOISELLE Eau de Parfum', brand: 'CHANEL' , image: 'https://www.sephora.com/productimages/sku/s513168-main-zoom.jpg?imwidth=162')
+perfume2 = Perfume.create(name: 'Flowerbomb', brand: 'Viktor&Rolf' , image: 'https://www.sephora.com/productimages/sku/s1377159-main-zoom.jpg?imwidth=162')
+perfume3 = Perfume.create(name: 'Libre Eau De Parfum', brand: 'Yves Saint Laurent', image: 'https://www.sephora.com/productimages/sku/s2249738-main-zoom.jpg?imwidth=162')
+perfume4 = Perfume.create(name: 'Daisy', brand: 'Marc Jacobs Fragrances', image: 'https://www.sephora.com/productimages/sku/s1029958-main-zoom.jpg?imwidth=162')
+perfume5 = Perfume.create(name: 'Chloé Eau de Parfum', brand: 'Chloé' , image: 'https://www.sephora.com/productimages/sku/s1239813-main-zoom.jpg?pb=2020-03-allure-best-2018&imwidth=162')
+
+# Review
+review1 = Review.create(perfume_id: 1, body: "This is my favorite perfume!")
+review2 = Review.create(perfume_id: 2, body: "Gorgeous perfume! It has a luxurious and classy scent")
+review3 = Review.create(perfume_id: 3, body: "I have been wearing this scent everyday for 4 years and it is still my absolute favorite.")
+review4 = Review.create(perfume_id: 4, body: "I love this scent! Long lasting and you only need a couple of sprays")
